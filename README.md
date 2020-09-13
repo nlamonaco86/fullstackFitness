079d4c70fac62f3da47a1ea5a395b5e5886d1722
Nicholas La Monaco <nlamonaco86@gmail.com>
2020-09-12 22:27:32 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# fullstack Fitness
+
+## Description
+
+fullstack Fitness is a comprehensive workout generator, planner, and exercise database, made using Node.js, Express, MySQL, Sequelize, Passport, Handlebars, JQuery and Bootstrap. 
+
+Deployed Site: coming soon!
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+
+## Installation
+
+Simply clone the repo, and/or visit the deployed page link to begin.
+
+## Usage
+
+## Questions
+
+Contact [Nlamonaco86](mailto:nlamonaco86@gmail.com) with any questions or suggestions!
---
+++ server.js
@@ -0,0 +1,38 @@
+// Dependencies
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+const app = express();
+const session = require("express-session");
+// Requiring passport as we've configured it
+const passport = require("./config/passport");
+
+// Serve static content for the app from the "public" directory
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Use sessions to keep track of user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set Handlebars
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import the models folder
+const db = require("./models");
+
+//Use the given routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+// Start the server so it can listening to client requests.
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("🌎 Now working at: http://localhost:" + PORT);
+  });
+});
